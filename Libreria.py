e9952a76dc4bd1440fbd521661be9aadfab8495f
yoroih <roycristhopherqp@gmail.com>
2022-11-03 19:03:06 -0500

    comit antes de 1.00

--- Libreria.py
+++ Libreria.py
@@ -110,68 +110,35 @@ def libreria():
         datos = pd.read_csv("libros.csv")
         #print(datos.sort_values(by="id"))
         print(datos.iloc[0:3])
+        
+    def listar(self):
+        datos = pd.read_csv("libros.csv")
+        print(datos.iloc[:,[1,2,3,4,5]])
+            
+    def add(self):
+        insert = True
+        while insert:
+            Id = input("Ingrese ID: ")
+            nombre = input("Ingresar nombre: ")
+            genero = input("Ingresar genero: ")
+            isbn = input("Ingresar ISBN: ") 
+            editorial = input("Ingrese Editorial: ")
+            autor = input("Ingrese autor: ")
+            lib_atributos = { "ID":Id ,"Titulo":nombre,"Genero":genero,"ISBN":isbn,"Editorial":editorial, "Autor":autor}
+            self.dicc_libros[nombre] = lib_atributos # Agrega el elemento al diccionario
+            print()
+            if (input("Registrar otro libro? S/N: ")).lower() == "n":
+                insert = False
 
-  def listar():
-      datos = pd.read_csv("libros.csv")
-      print(datos.iloc[:,[1,2,3,4,5]])
-  
-  print(menu())
-  opcion = int(input("Ingresa una opcion: ")) 
-  while True:
-    if  opcion == 1:
-      leer_archivo()
-      opcion2 = input("Deseas volver al menu? S/N: " ).upper()
-      if opcion2 == "S" or opcion2 == "SI":
-        print(menu())
-        opcion = int(input("Ingresa una opcion: "))
-      else:
-        print("Gracias por usar la libreria virtual")
-        break
-    if  opcion == 2:
-      listar()
-      opcion2 = input("Deseas volver al menu? S/N: " ).upper()
-      if opcion2 == "S" or opcion2 == "SI":
-        print(menu())
-        opcion = int(input("Ingresa una opcion: "))
-      else:
-        print("Gracias por usar la libreria virtual")
-        break
-    # if opcion == 3:
-    #   menu()
-    
-print(libreria())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  # def add(self):
-  #       insert = True
-  #       while insert:
-  #           Id = input("Ingrese ID: ")
-  #           nombre = input("Ingresar nombre: ")
-  #           genero = input("Ingresar genero: ")
-  #           isbn = input("Ingresar ISBN: ") 
-  #           editorial = input("Ingrese Editorial: ")
-  #           autor = input("Ingrese autor: ")
-  #           lib_atributos = { "ID":Id ,"Titulo":nombre,"Genero":genero,"ISBN":isbn,"Editorial":editorial, "Autor":autor}
-  #           self.dicc_libros[nombre] = lib_atributos # Agrega el elemento al diccionario
-  #           print()
-  #           if (input("Registrar otro libro? S/N: ")).lower() == "n":
-  #               insert = False
-
-  
+    def mostrar(self):
+        print()
+        for nombre, valor in self.dicc_libros.items(): # .items() funciona en Python 3.x
+            print("----")
+            Id = valor["ID"]
+            genero = valor["Genero"]
+            isbn = valor["ISBN"]
+            editorial = valor["Editorial"]
+            autor = valor["Autor"]
+            print("ID: {} | Nombre: {} | Genero: {} | ISBN: {} | Autor: {} | Editorial: {} ".format(Id,nombre,genero,isbn,autor, editorial))
+            
+            
